cd9fc3eb0d3f0122a0c1a38aa170aa38f9311e61
MLoretto <loreto.henriquez.m@gmail.com>
2018-08-24 13:17:44 -0300

    Prueba Functión Recursiva

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ let args = process.argv.slice(2);
 console.dir(args);
 const Marked = require('marked');
 const path = require('path');
+const fs = require('fs'); 
 
 let mdLinks = {};
+
+mdLinks.getDirectoryList = (myPath, files) => {
+	fs.readdirSync(myPath).forEach(function(file){
+		let subpath = path.join(myPath , file);
+        if(fs.lstatSync(subpath).isDirectory()){
+			//console.log(myPath + '/' + subpath);
+            mdLinks.getDirectoryList(subpath, files);
+        } else {
+            files.push(path.resolve(path.join(myPath),file));
+			//console.log(myPath + '/' + file);
+        }
+	});
+};
+
 mdLinks.verifyEntryPath = (path) => {
     if(path !== ''){
         return true;
@@ -58,7 +73,6 @@ function truncate(string, largo){
 if(args.length === 0 || args[0] === ""){
 	console.log('Tiene ingresar el path a revisar.');	
 }else{
-let fs = require('fs'); 
  
 fs.readdir(args[0], function(err, items) {
     console.log('Archivos:');
